2351434641c8fb1a5ac3f8b935c65d6ed9ad7507
kiyansh26 <kiyanshagarwal026@gmail.com>
2026-04-18 20:39:31 +0530

    18thApr

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -9,6 +9,3 @@ console.log(student.name)
 student.rollnumber= 20
 console.log(student.rollnumber)
 
-console.log(Math.sqrt(81))
-console.log(Math.max(10 , 50 , 60))
-console.log(Math.min(10 , 20 , 80))
